afbf405fa07688f772fb9d9c3dde1d336d80bd1c
QUINN LEE <zaac2240632@yahoo.com.tw>
2024-06-07 15:09:35 +0800

    projectDTO

---
+++ BoulevardOfBrokenDreams/Models/DTO/ProjectCardDTO.cs
@@ -0,0 +1,21 @@
+
+﻿namespace BoulevardOfBrokenDreams.Models.DTO
+{
+    public class ProjectCardDTO
+    {
+        public int ProjectId { get; set; }
+        public int MemberId { get; set; }
+        public decimal ProjectGoal { get; set; }
+
+        public decimal Total {  get; set; }
+        public string? ProjectName { get; set; }
+        public string? ProjectDescription { get; set; }
+        public string?  Thumbnail { get; set; }
+        public MemberDTO? Member { get; set; }
+        public ICollection<ProductCardDTO>? Products { get; set; }
+
+        public List<int>? ProductInCart { get; set; }
+
+        public List<int>? ProductInCartCount { get; set; }
+    }
+}
