565f62851525bf0618813d1835fe17d27fe181b0
QUINN LEE <zaac2240632@yahoo.com.tw>
2024-06-07 15:53:10 +0800

    fix cart

---
+++ BoulevardOfBrokenDreams/Models/DTO/ProductCardDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace BoulevardOfBrokenDreams.Models.DTO
+{
+    public class ProductCardDTO
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public int InitialStock { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int CurrentStock { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public string? Thumbnail { get; set; }
+
+    }
+}
---
+++ BoulevardOfBrokenDreams/Models/DTO/ProductDataInCartDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace BoulevardOfBrokenDreams.Models.DTO
+{
+    public class ProductDataInCartDTO
+    {
+        public string? Thumbnail { get; set; }
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+        public int CurrentStock { get; set; }
+
+
+        public int? Count { get; set; }
+
+
+
+        //public DateOnly StartDate { get; set; }
+        //public DateOnly EndDate { get; set; }
+        //public string? ProductDescription { get; set; }
+        //public int InitialStock { get; set; }
+    }
+}
