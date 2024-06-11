1b4259834fead608a81cd9d3d7bde33304a37a7d
steven Liao <stevnliao@gmail.com>
2024-06-11 16:07:18 +0800

    移植到ProjectInfoController

---
+++ BoulevardOfBrokenDreams/Models/DTO/VMProjectInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace BoulevardOfBrokenDreams.Models.DTO
+{
+    public class VMProjectInfo
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; } = null!;
+        public string? ProjectThumbnail { get; set; }
+        public string? ProjectDescription { get; set; }
+        public decimal ProjectGoal { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+        public int MemberId { get; set; }
+
+        //public int StatusId { get; set; }
+        //public string? GroupName { get; set; }
+        //public string? MemberName { get; set; }
+        //public string? MemberThumbnail { get; set; }
+        //public string? StatusName { get; set; }
+    }
+}
