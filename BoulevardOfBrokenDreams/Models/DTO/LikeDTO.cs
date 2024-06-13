d56e6b1389ab652488ce953ea845c5b5df7cd7ef
volt <z83420123@gmail.com>
2024-06-13 16:00:59 +0800

    merge to main

--- BoulevardOfBrokenDreams/Models/DTO/LikeDTO.cs
+++ BoulevardOfBrokenDreams/Models/DTO/LikeDTO.cs
@@ -4,9 +4,7 @@
     {
         public string LikePrjName { get; set; }
         public string LikePrjThumb { get; set; }
-
         public int LikePrjId { get; set; }
-
         public int LikeDetailId { get; set; }
     }
 }
