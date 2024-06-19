eba2c8507f220261e67bffed811a28987657f363
Akashi <a9135787@gmail.com>
2024-06-19 17:11:28 +0800

    6/19 load edit prj

--- BoulevardOfBrokenDreams/Controllers/HomeController.cs
+++ BoulevardOfBrokenDreams/Controllers/HomeController.cs
@@ -4,10 +4,12 @@ using BoulevardOfBrokenDreams.Models;
 using BoulevardOfBrokenDreams.Models.DTO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.IO;
+using System.Linq.Dynamic.Core;
 using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -172,9 +174,29 @@ namespace BoulevardOfBrokenDreams.Controllers
             return searchProjectDTO;
         }
 
+        [HttpGet("GetEditProject/{id}")]
+        public IEnumerable<GetEditProjectDTO> GetEditProject(int id)
+        {
+            var p = from x in _db.Projects.Where(x => x.ProjectId == id)
+                    join y in _db.ProjectIdtypes.Where(x => x.ProjectId == id)
+                    on x.ProjectId equals y.ProjectId
+                    select new GetEditProjectDTO()
+                    {
+                        Description = x.ProjectDescription,
+                        ProjectTypeId = y.ProjectTypeId,
+                        EndDate = x.EndDate,
+                        StartDate = x.StartDate,
+                        ProjectDetails = x.ProjectDetails,
+                        ProjectGoal = x.ProjectGoal,
+                        ProjectName = x.ProjectName,
+                        thumbnail = x.Thumbnail
+                    };
+            return p;
+        }
+
         // POST api/<HomeController>
         [HttpPost("CreateProject"), Authorize(Roles = "user")]
-        public async Task< IActionResult> CreateProject(CreateProjectDTO value)
+        public async Task<IActionResult> CreateProject(CreateProjectDTO value)
         {
             string? jwt = Request.Headers.Authorization;
             int memberId = DecodeJwtToMemberId(jwt);
@@ -192,7 +214,7 @@ namespace BoulevardOfBrokenDreams.Controllers
                 ProjectDetails = value.ProjectDetail,
             };
             var pj = _db.Projects.Add(project);
-            var isNull = _db.GroupDetails.Where(x=>x.GroupId == 1&&x.MemberId==memberId).IsNullOrEmpty();
+            var isNull = _db.GroupDetails.Where(x => x.GroupId == 1 && x.MemberId == memberId).IsNullOrEmpty();
             if (isNull)
             {
                 var groupdetails = new GroupDetail()
@@ -203,8 +225,8 @@ namespace BoulevardOfBrokenDreams.Controllers
                 };
                 _db.GroupDetails.Add(groupdetails);
             }
-            _db.SaveChanges();
-            int newPjId = pj.Entity.ProjectId;
+            await _db.SaveChangesAsync();
+            int newPjId = pj.Entity.ProjectId;//取得新增的id
             var type = new ProjectIdtype { ProjectId = newPjId, ProjectTypeId = Convert.ToInt32(value.ProjectTypeId) };
             _db.ProjectIdtypes.Add(type);
             if (newPjId > 0 && value.thumbnail.OpenReadStream() != null)
@@ -227,7 +249,7 @@ namespace BoulevardOfBrokenDreams.Controllers
 
 
                     project.Thumbnail = $"https://cdn.mumumsit158.com/{key}"; // 設置圖片路徑
-                    _db.SaveChanges(); // 再次保存更改
+                    await _db.SaveChangesAsync(); // 再次保存更改
                 }
 
 
---
+++ BoulevardOfBrokenDreams/Models/DTO/GetEditProjectDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace BoulevardOfBrokenDreams.Models.DTO
+{
+    public class GetEditProjectDTO
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public decimal ProjectGoal { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set;}
+        public string ProjectDetails { get; set; }
+        public string thumbnail { get; set; }
+        public int ProjectTypeId { get; set; }
+    }
+}
