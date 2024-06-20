7716d5a0f3bc5a744d1c60f3204e9c85728c424c
steven Liao <stevnliao@gmail.com>
2024-06-20 20:55:01 +0800

    改寫CommentsHub可接收留言，存入資料庫，並發送回連線的客戶端

--- BoulevardOfBrokenDreams/Hubs/CommentsHub.cs
+++ BoulevardOfBrokenDreams/Hubs/CommentsHub.cs
@@ -1,12 +1,54 @@
-﻿using BoulevardOfBrokenDreams.Models.DTO;
+﻿using Azure.Core;
+using BoulevardOfBrokenDreams.Models;
+using BoulevardOfBrokenDreams.Models.DTO;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 namespace BoulevardOfBrokenDreams.Hubs
 {
     public class CommentsHub:Hub
     {
+        private readonly MumuDbContext _context;
+
+        public CommentsHub(MumuDbContext mumuDbContext)
+        {
+            _context = mumuDbContext;
+        }
         public async Task SendMessage(CommentDto comment)
         {
+            // 取得傳送訊息者資料
+            var httpContext = Context.GetHttpContext();
+            if (httpContext == null) return;
+            string? jwt = httpContext.Request.Headers.Authorization;
+            if (string.IsNullOrEmpty(jwt)) return;
+            int memberId = DecodeJwtToMemberId(jwt);
+            
+            if(comment.CommentMsg == null) return;
+
+            // 存入這筆留言到資料庫
+            var dbComment = new Comment
+            {
+                CommentMsg = comment.CommentMsg,
+                ProjectId = comment.ProjectId,
+                MemberId = memberId,
+                Date = new DateTime(),
+                ParentId = comment.ParentId??null
+            };
+            await _context.Comments.AddAsync(dbComment);
+
+            // 發送到所有客戶端
             await Clients.All.SendAsync("ReceiveComment", comment);
+        }
+
+        private static int DecodeJwtToMemberId(string? jwt)
+        {
+            jwt = jwt.Replace("Bearer ", "");
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken decodedToken = tokenHandler.ReadJwtToken(jwt);
+            string? id = decodedToken.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return int.Parse(id!);
         }
     }
 }
